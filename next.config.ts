d7eba2200ee2151fc7001100948024dab5ec871b
Olutunmise Adeniji <tadeniji06@gmail.com>
2025-12-12 09:09:40 +0100

    feat: upgrade Next.js to version 16.0.10 for improved performance and features

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 	/* config options here */
-	eslint: {
-		ignoreDuringBuilds: true,
-	},
+	// eslint: {
+	// 	ignoreDuringBuilds: true,
+	// },
 	typescript: {
 		ignoreBuildErrors: true,
 	},
 		images: {
 		remotePatterns: [{ hostname: "cdn.sanity.io" }],
+		unoptimized: true,
 	},
 };
 
